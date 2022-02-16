1f6cfb34f6fc70006e4dffde47b3822aeaa24798
Sharad Vikram <sharad.vikram@gmail.com>
2022-02-15 20:29:11 -0800

    use neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations.rocinante = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, neovim-nightly-overlay, home-manager }:
+    let
       system = "x86_64-linux";
+    in {
+    nixosConfigurations.rocinante = nixpkgs.lib.nixosSystem {
+      inherit system;
       modules = [
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [
+            (import neovim-nightly-overlay)
+          ];
+        })
         ./configuration.nix
         ./hosts/rocinante.nix
         home-manager.nixosModules.home-manager {
